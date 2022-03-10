7da37b9a3df9d0969c8025d6bd2a9b83cc49b4e7
21bca06 <96610663+21bca06@users.noreply.github.com>
2022-03-10 11:13:37 +0530

    Solution3 uploaded

---
+++ SOLUTION/SOLUTION3.py
@@ -0,0 +1,6 @@
+#WAS to enter any number and check it is even or odd.
+num=int(input("ENTER ANY NUMBER:"))
+if(num%2==0):
+    print("{} Number is even".format(num))
+else:
+    print("{} Number is odd".format(num))
