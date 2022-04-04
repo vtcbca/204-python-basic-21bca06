ff72c354d2f77886128719faf793aaeabec2fe0b
21bca06 <96610663+21bca06@users.noreply.github.com>
2022-04-04 10:56:01 +0530

    Add files via upload
    SOLUTION UPLOAD

--- SOLUTION/SOLUTION15.py
+++ SOLUTION/SOLUTION15.py
@@ -1,15 +1,7 @@
-"""WAS to print following pattern
-    1
-    2  3
-    4  5  6
-    7  8  9  10
-    11 12 13 14 15
-    16 17 18 19 20 21 """
-    
-i=6
+n=int(input("Enter any number:"))
 no=1
-for a in range(1,i+1):
-    for b in range(1,a+1):
+for a in range(0,n):
+    for b in range(0,a+1):
         print(no,end=' ')
         no=no+1
     print()
